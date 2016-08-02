b00ee7a31b7ad27c52f49ea1d6f03fbb7c4d7ced
Poppe <stefan.poppe@axelspringer.com>
2016-08-02 13:50:12 +0200

    DooBee Commit

---
+++ src/DooBee.java
@@ -0,0 +1,22 @@
+/**
+ * Created by spoppe on 02.08.2016.
+ */
+
+public class DooBee {
+
+
+    public static void main(String[] args) {
+        int x = 1;
+
+        while (x < 3) {
+            System.out.println("Doo");
+            System.out.println("Bee");
+            x++;
+        }
+
+        if (x == 3) {
+            System.out.println("Do");
+        }
+
+    }
+}
